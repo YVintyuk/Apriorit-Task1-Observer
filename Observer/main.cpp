077faba390af330f5860a9c069feda54060e9f4f
Yuliana Vintyuk <yuliana.vintyuk@gmail.com>
2022-05-09 21:19:37 +0200

    Initial Commit

---
+++ Observer/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Observer
+//
+//  Created by Yuliana Vintyuk on 09.05.22.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
